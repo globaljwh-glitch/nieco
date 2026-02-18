ef4b62c341a654ce36fddf34f8612d9320e16690
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-02-18 16:53:39 +0530

    migration files changes

--- database/migrations/2026_02_17_114742_add_additional_fields_to_contacts_table.php
+++ database/migrations/2026_02_17_114742_add_additional_fields_to_contacts_table.php
@@ -31,7 +31,6 @@ return new class extends Migration
                   ->after('use_of_product');
         });
     }
-
     public function down(): void
     {
         Schema::table('contacts', function (Blueprint $table) {
