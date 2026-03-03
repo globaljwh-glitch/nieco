73951602a433a89d0be172819d5bf727a4d701ee
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-03-03 14:34:42 +0530

    fixing issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'api/contact-submit',
+            'jobs/*/apply',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
