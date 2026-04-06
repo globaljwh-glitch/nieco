52fdaf0f80a251cd490871a5c0dab93fe81073c3
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-04-06 16:19:22 +0530

    email test

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -16,27 +16,90 @@ class ContactController extends Controller
      * Create mail test function
      */
     public function testEmail(Request $request) {
-        $to = "ramankant.vashisht@jwhglobal.com"; // change this
-        $subject = "Test Mail";
-        $message = "If you see this, mail() is working.";
-        $headers = "From: ramankant.vashisht@jwhglobal.com\r\n";
-        $headers .= "Reply-To: ramankant.vashisht@jwhglobal.com\r\n";
-        $headers .= "X-Mailer: PHP/" . phpversion();
 
-        if (mail($to, $subject, $message, $headers)) {
-            //echo "Mail sent successfully";
-            return response()->json([
+        $server = "10.1.15.156";
+$port = 25;
+ 
+$from = "test@unitedfoodscorp.com";
+$to = "ramankantmca@gmail.com"; // change this
+$subject = "SMTP Test Mail";
+$message = "This is a test email sent using raw SMTP in PHP.";
+ 
+$timeout = 30;
+ 
+// Open socket
+$socket = fsockopen($server, $port, $errno, $errstr, $timeout);
+ 
+if (!$socket) {
+    die("Connection failed: $errstr ($errno)");
+}
+ 
+// Start SMTP conversation
+echo getResponse($socket);
+ 
+sendCommand($socket, "HELO localhost");
+sendCommand($socket, "MAIL FROM:<$from>");
+sendCommand($socket, "RCPT TO:<$to>");
+sendCommand($socket, "DATA");
+ 
+// Email headers + body
+$data = "Subject: $subject\r\n";
+$data .= "From: $from\r\n";
+$data .= "To: $to\r\n";
+$data .= "\r\n$message\r\n.";
+ 
+sendCommand($socket, $data);
+ 
+sendCommand($socket, "QUIT");
+ 
+fclose($socket);
+ 
+//echo "\nEmail process completed.\n";
+
+return response()->json([
                 'success' => true,
                 'message' => 'Mail sent successfully.'
             ], 200);
-        } else {
-            //echo "Mail failed";
-            return response()->json([
-                'success' => true,
-                'message' => 'Mail failed.'
-            ], 200);
-        }
+        // $to = "ramankant.vashisht@jwhglobal.com"; // change this
+        // $subject = "Test Mail";
+        // $message = "If you see this, mail() is working.";
+        // $headers = "From: ramankant.vashisht@jwhglobal.com\r\n";
+        // $headers .= "Reply-To: ramankant.vashisht@jwhglobal.com\r\n";
+        // $headers .= "X-Mailer: PHP/" . phpversion();
+
+        // if (mail($to, $subject, $message, $headers)) {
+        //     //echo "Mail sent successfully";
+        //     return response()->json([
+        //         'success' => true,
+        //         'message' => 'Mail sent successfully.'
+        //     ], 200);
+        // } else {
+        //     //echo "Mail failed";
+        //     return response()->json([
+        //         'success' => true,
+        //         'message' => 'Mail failed.'
+        //     ], 200);
+        // }
     }
+
+    // Function to read response
+public function getResponse($socket) {
+    $response = '';
+    while ($str = fgets($socket, 515)) {
+        $response .= $str;
+        if (substr($str, 3, 1) == " ") break;
+    }
+    return $response;
+}
+ 
+// Function to send command
+public function sendCommand($socket, $command) {
+    echo ">> $command\n";
+    fputs($socket, $command . "\r\n");
+    $response = getResponse($socket);
+    echo "<< $response\n";
+    return $response;
+}
 
     public function store(Request $request)
     {
